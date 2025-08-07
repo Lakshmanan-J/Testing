7a93f8f8561db75f9199c891004aa496d2a37b20
Lakshmanan <lache@gmail.com>
2025-08-07 12:52:20 +0530

    Updated index.html

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
             <h6>Vivo</h6>
             <h6>Realme P2</h6>
             <h6>Oppo</h6>  
-            <h6>Nothing</h6>     
+            <h6>Nothing</h6> 
+            <h6>oneplus</h6>     
         </body>
     </head>
 </html>
