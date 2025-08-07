750c98e7fe08514e19185dc324c5add818572985
Lakshmanan-J <jlakshmananbca@gmail.com>
2025-08-07 12:56:21 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,7 @@
             <h6>Vivo</h6>
             <h6>Realme P2</h6>
             <h6>Oppo</h6>  
-            <h6>Nothing</h6> 
-            <h6>oneplus</h6>     
+            <h6>Nothing</h6>     
         </body>
     </head>
 </html>
